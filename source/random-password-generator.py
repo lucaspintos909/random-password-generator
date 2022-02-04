d7d98ef9bdee020225abb59a6226c0fbd3592943
Lucas Pintos <lucaspintos909@gmail.com>
2022-02-03 21:10:24 -0300

    Función para crear la contraseña aleatoria

---
+++ source/random-password-generator.py
@@ -0,0 +1,30 @@
+import random
+import string
+
+def generateRandomPassword(pass_len, num_len, sym_len):
+
+    # Concateno las letras en minúscula y en mayúscula
+    letters = string.ascii_lowercase + string.ascii_uppercase
+
+    # Calculo la cantidad de letras en base a los numero y simbolos solicitados
+    number_of_letters = pass_len - (num_len + sym_len)
+
+    # Obtengo una lista con la cantidad de letras aleatorias
+    letters = random.sample(letters, number_of_letters)
+
+    # Obtengo una lista con la cantidad de numeros aleatorios
+    numbers = random.sample(string.digits, num_len)
+
+    # Obtengo una lista con la cantidad de simbolos aleatorios
+    symbols = random.sample(string.punctuation, sym_len)
+
+    # Junto las tres listas
+    password = letters + numbers + symbols
+
+    # Mezclo la lista
+    random.shuffle(password)
+
+    # Uno todos los caracteres de la lista
+    password = ''.join(password)
+
+    return password
